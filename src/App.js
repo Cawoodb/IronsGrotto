1de4934d9add9624fff932cb0b8137651aca336a
JohnBooneDev <JohnBooneDev@Gmail.com>
2020-08-04 19:55:30 -0400

    slightly better temp page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import './App.css';
 import { Redirect } from 'react-router-dom'
+import './temp.css'
 
 
 function App() {
   return (
     <div className="App">
-      <a href="https://discord.gg/tJE8u3r"> Irons Grotto Discord</a>
+      <ul class="tilesWrap">
+        <li>
+          <h2>N</h2>
+          <h3>Discord</h3>
+          <p>
+            Our place to chat and meme with eachother
+          </p>
+          <form action="https://discord.gg/tJE8u3r">
+              <button type="submit">Join Discord</button>
+          </form>
+        </li>
+        <li>
+          <h2>O</h2>
+          <h3>Twitter</h3>
+          <p>
+            We write stuff sometimes
+          </p>
+          <form action="https://twitter.com/ironsgrotto">
+              <button type="submit">Tweets</button>
+          </form>
+        </li>
+        <li>
+          <h2>O</h2>
+          <h3>Forums</h3>
+          <p>
+            Our section in the official RuneScape forums
+          </p>
+          <form action="https://shorturl.at/hwGI6">
+            <button type="submit">Bump</button>
+          </form>
+        </li>
+        <li>
+          <h2>B</h2>
+          <h3>Temple</h3>
+          <p>
+            A place to track clan gains and competitions
+          </p>
+          <form action="https://shorturl.at/nsDHP">
+            <button type="submit">XP</button>
+          </form>
+        </li>
+      </ul>
     </div>
   );
 }
