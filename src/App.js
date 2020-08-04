a51f1b21c9157c23bbaaff03efe3fdfaab244d04
JohnBooneDev <JohnBooneDev@Gmail.com>
2020-08-04 18:51:41 -0400

    quick fix for discord link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Redirect } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="https://discord.gg/tJE8u3r"> Irons Grotto Discord</a>
     </div>
   );
 }
